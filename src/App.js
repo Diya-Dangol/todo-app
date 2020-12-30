f1ab728e3d2b837ad956bcdd570497ba33f9f220
Diya-Dangol <diya.dangol15@gmail.com>
2020-12-30 12:41:29 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import TodoHome from './component/todoHome'
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Router>
-       <Route path='/' exact>
-         <TodoHome />
-       </Route>
-     </Router>
+    <div className="App">    
+      <TodoHome />       
     </div>
   );
 }
